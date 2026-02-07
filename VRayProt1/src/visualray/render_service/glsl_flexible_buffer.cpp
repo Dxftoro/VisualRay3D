8aec10a380843fe7dff431212a90e4d015d099aa
Ftoro <lesnik.antonio@gmail.com>
2026-02-07 21:08:52 +0300

    feat: flexible BO controller realization

---
+++ VRayProt1/src/visualray/render_service/glsl_flexible_buffer.cpp
@@ -0,0 +1,50 @@
+#include "vrpch.h"
+#include "glsl_flexible_buffer.h"
+
+#include "logservice.h"
+#include <glad/glad.h>
+
+namespace vray {
+
+	void GlslBufferController::init(GLuint& handle, GLenum type, const void* data, size_t size, GlslUsage usage) {
+		glGenBuffers(1, &handle);
+		glBindBuffer(type, handle);
+			glBufferData(type, size, data, (GLenum)usage);
+		glBindBuffer(type, 0);
+	}
+
+	void GlslBufferController::resize(GLuint& handle, GLenum type, size_t size, GlslUsage usage) {
+		GLuint newBuffer;
+		glGenBuffers(1, &newBuffer);
+		glBindBuffer(GL_COPY_READ_BUFFER, handle);
+		glBindBuffer(GL_COPY_WRITE_BUFFER, newBuffer);
+
+		glBufferData(GL_COPY_WRITE_BUFFER, size, nullptr, (GLenum)usage);
+
+		if (handle) {
+			glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_COPY_WRITE_BUFFER, 0, 0, size);
+			glDeleteBuffers(1, &handle);
+		}
+
+		handle = newBuffer;
+	}
+
+	void GlslBufferController::free(GLuint& handle) {
+		glDeleteBuffers(1, &handle);
+	}
+
+	void GlslBufferController::bind(GLuint handle, GLenum type) {
+		glBindBuffer(type, handle);
+	}
+
+	void GlslBufferController::unbind(GLenum type) {
+		glBindBuffer(type, 0);
+	}
+
+	void GlslBufferController::setSubData(GLuint handle, GLenum type, const void* data, size_t size, GlslUsage usage) {
+		glBindBuffer(type, handle);
+		glBufferData(type, size, data, (GLenum)usage);
+		glBindBuffer(type, 0);
+	}
+
+}
--- VRayProt1/src/visualray/render_service/glsl_flexible_buffer.h
+++ VRayProt1/src/visualray/render_service/glsl_flexible_buffer.h
@@ -1,18 +1,82 @@
 #pragma once
-//#include "kernel.h"
-//
-//template <GLenum BufferType, typename DataType>
-//class GlslFlexibleBuffer {
-//private:
-//	int capacity, extent, size;
-//
-//public:
-//	GlslFlexibleBuffer();
-//	GlslFlexibleBuffer(int initialSize);
-//
-//	int getCapacity() const { return capacity; }
-//	int getExtent() const { return extent; }
-//	int getSize() const { return size; }
-//
-//	void setExtent(int extent) { this->extent = extent; }
-//};
+#include "kernel.h"
+#include <type_traits>
+
+namespace vray {
+
+	enum class GlslUsage : GLenum {
+		STREAM_DRAW = 0x88E0,
+		STREAM_READ = 0x88E1,
+		STREAM_COPY = 0x88E2,
+		STATIC_DRAW = 0x88E4,
+		STATIC_READ = 0x88E5,
+		STATIC_COPY = 0x88E6,
+		DYNAMIC_DRAW = 0x88E8,
+		DYNAMIC_READ = 0x88E9,
+		DYNAMIC_COPY = 0x88EA
+	};
+
+	template <typename T>
+	concept GlslTrivial = std::is_trivially_copyable_v<T>;
+
+	class GlslBufferController {
+		template <GLenum BufferType, typename DataType> requires GlslTrivial<DataType>
+		friend class GlslFlexibleBuffer;
+
+	private:
+		static void init(GLuint& handle, GLenum type, const void* data, size_t size, GlslUsage usage);
+		static void resize(GLuint& handle, GLenum type, size_t size, GlslUsage usage);
+		static void free(GLuint& handle);
+
+		static void bind(const GLuint handle, GLenum type);
+		static void unbind(GLenum type);
+
+		static void setSubData(GLuint handle, GLenum type, const void* data, size_t size, GlslUsage usage);
+	};
+
+	template <GLenum BufferType, typename DataType> requires GlslTrivial<DataType>
+	class GlslFlexibleBuffer {
+	private:
+		size_t capacity, extent, _size;
+		GLuint handle;
+		GlslUsage usage;
+
+		GlslFlexibleBuffer(const GlslFlexibleBuffer&) = delete;
+		GlslFlexibleBuffer(GlslFlexibleBuffer&&) noexcept = delete;
+		GlslFlexibleBuffer& operator=(const GlslFlexibleBuffer&) = delete;
+
+	public:
+		static constexpr size_t defaultCapacity		= 10;
+		static constexpr size_t defaultExtent		= 10;
+
+		explicit GlslFlexibleBuffer(GlslUsage usage = GlslUsage::DYNAMIC_DRAW);
+		explicit GlslFlexibleBuffer(size_t initialSize, GlslUsage usage = GlslUsage::DYNAMIC_DRAW);
+		~GlslFlexibleBuffer();
+
+		size_t getCapacity() const { return capacity; }
+		size_t getExtent() const { return extent; }
+		size_t getSize() const { return _size; }
+		GlslUsage getUsage() const { return usage; }
+
+		inline size_t size() const { return getSize(); }
+
+		void setExtent(size_t extent) { this->extent = extent; }
+		void setUsage(GlslUsage usage) { this->usage = usage; }
+	};
+
+	template <GLenum BufferType, typename DataType> requires GlslTrivial<DataType>
+	GlslFlexibleBuffer<BufferType, DataType>::GlslFlexibleBuffer(GlslUsage usage) {
+
+	}
+
+	template <GLenum BufferType, typename DataType> requires GlslTrivial<DataType>
+	GlslFlexibleBuffer<BufferType, DataType>::GlslFlexibleBuffer(size_t initialSize, GlslUsage usage) {
+
+	}
+
+	template <GLenum BufferType, typename DataType> requires GlslTrivial<DataType>
+	GlslFlexibleBuffer<BufferType, DataType>::~GlslFlexibleBuffer() {
+
+	}
+
+}
