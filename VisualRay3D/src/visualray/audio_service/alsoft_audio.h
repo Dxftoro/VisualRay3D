38de8543ddadc40c9f859d1b0ea8eba45591bdf6
Ftoro <lesnik.antonio@gmail.com>
2026-06-06 21:01:43 +0300

    add: audio system FDs

--- VisualRay3D/src/visualray/audio_service/alsoft_audio.h
+++ VisualRay3D/src/visualray/audio_service/alsoft_audio.h
@@ -3,11 +3,17 @@
 
 namespace vray {
 
+	struct ALCdevice;
+	struct ALCcontext;
+
 	class AlsoftAudio : public Audio {
 	private:
+		ALCdevice* device;
+		ALCcontext* context;
+
 	public:
 		AlsoftAudio(entt::registry& world) : Audio(world) {}
-		~AlsoftAudio() override {}
+		~AlsoftAudio() override;
 
 		void update() override;
 	};
