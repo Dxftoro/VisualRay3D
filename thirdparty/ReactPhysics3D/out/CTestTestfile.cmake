550c8f7172fcb2861f396295c743aabd37ca18c8
Ftoro <lesnik.antonio@gmail.com>
2026-06-17 03:20:56 +0300

    build: removed extra cmake configs and projects from ReactPhysics3D

--- thirdparty/ReactPhysics3D/out/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Programming/MyPrograms/VisualRay3D/thirdparty/ReactPhysics3D
-# Build directory: D:/Programming/MyPrograms/VisualRay3D/thirdparty/ReactPhysics3D/out
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
