03904face649307a9b3c265e26e33d797b8d31e7
Ftoro <lesnik.antonio@gmail.com>
2026-08-07 08:12:28 +0300

    feat: completed ISpatial interface

--- VisualRay3D/src/visualray/world/spatial.h
+++ VisualRay3D/src/visualray/world/spatial.h
@@ -1,15 +1,11 @@
 #pragma once
 
 #include "aabb.h"
+#include "frustum.h"
 #include "../util/function_ref.h"
 #include "../thirdparty/EnTT/entt.hpp"
 
 namespace vray {
-
-	enum class SpatialType {
-		GRID,
-		NONE
-	};
 
 	class ISpatial {
 	public:
@@ -17,7 +13,10 @@ namespace vray {
 
 		virtual void insert(entt::entity entity) = 0;
 		virtual void remove(entt::entity entity) = 0;
-		virtual void queryFrustum() = 0;
+		virtual bool update(entt::entity, const Aabb& newAabb) = 0;
+		virtual void queryFrustum(const Frustum& frustum, FunctionRef<void(entt::entity)> callback) = 0;
+		virtual void queryAabb(const Aabb& aabb, FunctionRef<void(entt::entity)> callback) = 0;
+		virtual void querySphere(const glm::vec3& position, float radius, FunctionRef<void(entt::entity)> callback) = 0;
 	};
 
 }
