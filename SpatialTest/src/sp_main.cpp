83fb4d44617ee8444023a11c3189605b4e55c6a2
Ftoro <lesnik.antonio@gmail.com>
2026-08-13 08:48:42 +0300

    set a light map

--- SpatialTest/src/sp_main.cpp
+++ SpatialTest/src/sp_main.cpp
@@ -51,7 +51,7 @@ SpatialTest::SpatialTest()
 
 	spawner = new Spawner(game);
 
-	spawner->spawnMap(maps.get("heavy"), { 0.0f, 1.0f, 1.0f });
+	spawner->spawnMap(maps.get("light"), { 0.0f, 1.0f, 1.0f });
 
 	game.world.view<CompMapSpawn>().each([this] (entt::entity entity, CompMapSpawn& spawn) {
 		spawns.push_back(entity);
